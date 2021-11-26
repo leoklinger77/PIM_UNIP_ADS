b2642a20dde1886b7ec0887927b7ba0e11e28f16
Klinger <leandro.klingeroliveira@gmail.com>
2021-11-26 12:31:20 -0300

    Quebrando a solution em Desktop e MVC

--- HotelTransamerica/src/Mvc/UnipPim.Hotel.Pagamento.Dominio/Models/Produto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UnipPim.Hotel.Pagamento.Dominio.Models
-{
-    public class Produto
-    {
-        public string Nome { get; set; }
-        public int Quantidade { get; set; }
-        public decimal Valor { get; set; }
-    }
-}
--- HotelTransamerica/src/Mvc/UnipPim.Hotel.Pagamento.Dominio/Servico/IPagamentoCartaoCreditoFacade.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnipPim.Hotel.Pagamento.Dominio.Models;
-
-namespace UnipPim.Hotel.Pagamento.Dominio.Servico
-{
-    public interface IPagamentoCartaoCreditoFacade
-    {
-        Transacao RealizarPagamento(Pedido pedido, Models.Pagamento pagamento);
-    }
-}
